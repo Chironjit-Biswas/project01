ea3dca4897f133bf7399a5ef62a0eeccbcb14dcc
Chironjit-Biswas <chironjit2@gmail.com>
2024-09-25 01:14:59 +0600

    nav component add

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const Navbar = () => {
+    return (
+        <div>
+            <div>
+                <ul>
+                    <li><a href="http://">Home</a></li>
+                    <li><a href="http://">Product</a></li>
+                    <li><a href="http://">Contact</a></li>
+                    <li><a href="http://">About</a></li>
+                </ul>
+            </div>
+        </div>
+    )
+}
