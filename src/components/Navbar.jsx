eb89b90adfd566335b83fa3993ae4d126d41e574
Chironjit-Biswas <chironjit2@gmail.com>
2024-09-25 01:45:20 +0600

    nav component add and login & Register added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,6 +10,7 @@ export const Navbar = () => {
                     <li><a href="http://">Contact</a></li>
                     <li><a href="http://">About</a></li>
                 </ul>
+            </div>
                 <form>
                     <input type="text" placeholder="Search..."/>
                 </form>
